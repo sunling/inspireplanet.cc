b2af3c76894b952dee95875b08744e28d4cd19be
sunling <sunling621@gmail.com>
2025-04-25 14:22:16 -0700

    use puppeteer chrome

--- scripts/generate-from-airtable.js
+++ scripts/generate-from-airtable.js
@@ -8,20 +8,20 @@ const { fetchAirtableData, formatToBeijingTime } = require('./utils');
   const data = await fetchAirtableData();
   const template = fs.readFileSync('templates/card.html', 'utf8');
 
-  let executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-  if (!fs.existsSync(executablePath)) {
-    throw new Error('Chrome not found at expected path. Please edit generate.js to set correct path.');
-  }
-  const browser = await puppeteer.launch({
-    executablePath,
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-
+  // let executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+  // if (!fs.existsSync(executablePath)) {
+  //   throw new Error('Chrome not found at expected path. Please edit generate.js to set correct path.');
+  // }
   // const browser = await puppeteer.launch({
-  //   headless: 'new', // 使用 Puppeteer 内置的 Chromium
+  //   executablePath,
+  //   headless: true,
   //   args: ['--no-sandbox', '--disable-setuid-sandbox'],
   // });
+
+  const browser = await puppeteer.launch({
+    headless: 'new', // 使用 Puppeteer 内置的 Chromium
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
 
   const screenshotTasks = [];
 
